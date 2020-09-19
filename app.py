a7b79bb97564bd37a2637bbc1a64f7aec5b732df
Uday Jagtap <63186019+jagtapuday@users.noreply.github.com>
2020-09-19 17:03:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def Start(data):
     Main=Code.main_class()
     st.title("Gapminder Analysis(Kaggle)")
     st.sidebar.title("Analysis Based On")
-    Option=st.sidebar.selectbox("Select Option",("Select","General Overview","Based On Region","Based On Country"))
+    Option=st.sidebar.selectbox("Select Option",("Select","Based On Country","Based On Region","General Overview"))
     if Option=="Select":
         Main.main(data,"Select")
     elif Option=="General Overview":
@@ -27,12 +27,14 @@ def Start(data):
         Country = st.sidebar.selectbox("Select Country", tuple(Country))
         st.header("Analysis of " + Country )
         Main.main(data,"Based On Country",Country)
+        print("Done")
 
 if __name__=="__main__":
 
     @st.cache
     def load_dataset():
         data = pd.read_csv("clean_data.csv")
+        print("loaded")
         return data
     data = load_dataset()
     Start(data)
